70a8b324c24f0d6e1599aa89878a852ac370b340
Davis, John <JDavis@scrippsnetworks.com>
2019-03-06 15:10:19 -0500

    Initial commit from boydsters repo

---
+++ Themes/default/languages/Modifications.english.php
@@ -0,0 +1,29 @@
+<?php
+// Version: 2.0; Modifications
+
+//	Ban List
+$txt['banlist_menu'] = 'Banlist';
+$txt['permissionname_view_banlist'] = 'View the Ban List';
+$txt['permissionhelp_view_banlist'] = 'The banlist shows all members that have are banned on your forum. The list can be sorted.'; 
+$txt['cannot_view_banlist'] = 'You can\'t view the banlist because you don\'t have permission to.';
+$txt['whoallow_banlist'] = 'Viewing <a href="' . $scripturl . '?action=banlist">Banlist</a>.';
+$txt['mentions'] = 'Mentions';
+$txt['scheduled_task_removeMentions'] = 'Remove seen mentions';
+$txt['scheduled_task_desc_removeMentions'] = 'Automatically removes seen mentions older than the specified days';
+
+//Admin Ban Button	
+$txt['admin_ban_button'] = 'Ban user';
+
+// Begin Auto Embed Media Pro Text Strings
+$txt['mediapro_admin'] = 'Simple Audio Video Embedder';
+$txt['mediapro_settings'] = 'Settings';
+$txt['mediapro_save_settings'] = 'Save Settings';
+$txt['mediapro_err_cache'] = 'Cache folder not writable must fix for performance!';
+$txt['mediapro_settings2'] = 'Settings and configuration for Simple Audio Video Embedder';
+$txt['mediapro_copyremove'] = 'Copyright Removal';
+$txt['mediapro_copyremove2'] = 'Remove the copyright for Simple Audio Video Embedder';
+// END Auto Embed Media Pro  Text Strings			
+
+//Fix the "Unknown Action" message
+$txt['who_thisaction'] = 'Viewing the ';
+?>
